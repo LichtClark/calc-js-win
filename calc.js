b25307fbb7787d2a0f42e63c623b7c1ee7374635
LichtClark <41386665+LichtClark@users.noreply.github.com>
2021-12-16 18:21:18 +0100

    Update calc.js

--- calc.js
+++ calc.js
@@ -1,3 +1,3 @@
 var child = require('child_process').execFile;
-var executablePath = "/mnt/c/Windows/System32/calc.exe";
+var executablePath = "~/mnt/c/Windows/System32/calc.exe";
 child(executablePath, function(err, data) {});
